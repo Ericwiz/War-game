367629648bef4019f5f1d7a7aa9c273dc5d43ffe
Ericwiz <wisdomagu44@gmail.com>
2022-10-04 17:48:13 +0100

    Improved the user experience

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const playBtn = document.getElementById('btn-play');
+const parentElemt = document.getElementById('display-image');
+const winner = document.getElementById('winner');
 let deckId
 
 function getCard() {
@@ -20,13 +22,31 @@ document.getElementById('draw-cards').addEventListener('click', () => {
         .then(res => res.json())
         .then(data => {
             console.log(data)
-                let img = `
-                    <img src="${data.cards[0].image}" />
-                    <img src="${data.cards[1].image}" />
-                `
-                document.getElementById('display-image').innerHTML += img;
+            parentElemt.children[0].innerHTML = `<img src="${data.cards[0].image}" />`
+            
+            parentElemt.children[1].innerHTML = `<img src="${data.cards[1].image}" />`
+
+            determineCardWinner(data.cards[0], data.cards[1])
         })
     
 })
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    console.log("card 1:", card1ValueIndex)
+    console.log("card 2:", card2ValueIndex)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        return  winner.textContent = "Card 1 wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return winner.textContent = "Card 2 wins!"
+    } else {
+        return  winner.textContent = "War!"
+    }
+}
+
 
 
